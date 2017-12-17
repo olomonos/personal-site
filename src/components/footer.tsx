2ae4ee86b62002168c137fb1b39a1bee360b721f
olomonos <o.lomonos@gmail.com>
2017-12-17 15:06:21 +0200

    footer is modified;

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,31 +7,34 @@ export const Footer: React.StatelessComponent = () => {
 
     return (
         <footer className="page-footer">
-          <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">Languages</h5>
-                <ul>
-                  <li>Javascript</li>
-                </ul>
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Human Languages</h5>
-                <ul>
-                  <li>English</li>
-                  <li>German</li>
-                  <li>Ukrainian</li>
-                  <li>Russian</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="footer-copyright">
             <div className="container">
-            © {year} Olena Lomonos
-            {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+                <div className="row">
+                    <div className="col l6 s12">
+                        <h5 className="white-text">Languages</h5>
+                        <ul>
+                            <li>HTML5</li>
+                            <li>CSS3 / SASS</li>
+                            <li>Javascript / Typescript</li>
+                        </ul>
+                    </div>
+                    <div className="col l4 offset-l2 s12">
+                        <h5 className="white-text">Human Languages</h5>
+                        <ul>
+                            <li>English</li>
+                            <li>German</li>
+                            <li>Ukrainian</li>
+                            <li>Russian</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </div>
-        </footer>    
+            <div className="footer-copyright">
+                <div className="container">
+                    © {year} Olena Lomonos
+                    {/* © 2017 - {year} Olena Lomonos */}
+            {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+                </div>
+            </div>
+        </footer>
     );
 };
