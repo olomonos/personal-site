735f8b4a2b066b2334f1582ee70966f4a1c5997b
olomonos <o.lomonos@gmail.com>
2017-12-17 14:24:28 +0200

    footer is added;

--- src/layouts/home.tsx
+++ src/layouts/home.tsx
@@ -5,7 +5,6 @@ import { Footer } from '../components/footer';
 export const Home: React.StatelessComponent = () => (
     <div className="home">
         <MainCard />
-        <a className="waves-effect waves-light btn">button</a>
         <Footer />
     </div>
 );
