0bc90afbd719287286902516bba0a934741d50d3
olomonos <o.lomonos@gmail.com>
2017-12-17 14:40:20 +0200

    sass classes are reorganized;

--- src/components/main-card.tsx
+++ src/components/main-card.tsx
@@ -3,7 +3,7 @@ import { NAME, EMAIL, GITHUB } from '../data/main-card';
 
 export const MainCard: React.StatelessComponent = () => {
     return (
-        <div className="card-panel bg-color">
+        <div className="card-panel">
             <h2 className="main-card-text">{NAME}</h2>
             <a className="main-card-text" href="https://github.com/olomonos/">{GITHUB}</a>
             <h6 className="main-card-text">{EMAIL}</h6>
