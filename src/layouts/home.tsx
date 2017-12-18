de3074259b46bc51b38f70ef077597d4501313a7
olomonos <o.lomonos@gmail.com>
2017-12-18 17:41:05 +0200

    edition 2 is ready;

--- src/layouts/home.tsx
+++ src/layouts/home.tsx
@@ -12,39 +12,30 @@ export const Home: React.StatelessComponent = () => (
             <MainCard />
             {/* <Projects /> */}
             
-            {/* <div className="div-img color-3" /> */}
             <ProjectTab
                 projectName={PROJECT_1.projectName}
                 projectDescription={PROJECT_1.projectDescription}
                 githubLink={PROJECT_1.githubLink}
                 githubPageLink={PROJECT_1.githubPageLink}
             />
-
-            {/* <div className="div-img color-2 tall-div" /> */}
             <ProjectTab
                 projectName={PROJECT_2.projectName}
                 projectDescription={PROJECT_2.projectDescription}
                 githubLink={PROJECT_2.githubLink}
                 githubPageLink={PROJECT_2.githubPageLink}
             />
-
-            {/* <div className="div-img color-5" /> */}
             <ProjectTab
                 projectName={PROJECT_3.projectName}
                 projectDescription={PROJECT_3.projectDescription}
                 githubLink={PROJECT_3.githubLink}
                 githubPageLink={PROJECT_3.githubPageLink}
             />
-
-            {/* <div className="div-img color-3 tall-div" /> */}
-            {/* <div className="div-img color-4" /> */}
             <ProjectTab
                 projectName={PROJECT_4.projectName}
                 projectDescription={PROJECT_4.projectDescription}
                 githubLink={PROJECT_4.githubLink}
                 githubPageLink={PROJECT_4.githubPageLink}
             />
-            {/* <div className="div-img color-5" /> */}
 
         </div>
         <Footer />
