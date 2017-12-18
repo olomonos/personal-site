c9ebadd4a9213a9815766dd31313dda39a98859e
olomonos <o.lomonos@gmail.com>
2017-12-18 17:16:40 +0200

    edition 2a is ready;

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { ProjectTab } from '../components/project-tab';
 import { PROJECT_1, PROJECT_2, PROJECT_3, PROJECT_4 } from '../data/projects';
 
+// delete this file ?
+
 export const Projects: React.StatelessComponent = () => {
     return (
         <div className="projects">
@@ -10,32 +12,24 @@ export const Projects: React.StatelessComponent = () => {
                 projectDescription={PROJECT_1.projectDescription}
                 githubLink={PROJECT_1.githubLink}
                 githubPageLink={PROJECT_1.githubPageLink}
-                className="color-1"
-                data-grid-class="item-1"
             />
             <ProjectTab
                 projectName={PROJECT_2.projectName}
                 projectDescription={PROJECT_2.projectDescription}
                 githubLink={PROJECT_2.githubLink}
                 githubPageLink={PROJECT_2.githubPageLink}
-                className="color-2"
-                data-grid-class="item-2"
             />
             <ProjectTab
                 projectName={PROJECT_3.projectName}
                 projectDescription={PROJECT_3.projectDescription}
                 githubLink={PROJECT_3.githubLink}
                 githubPageLink={PROJECT_3.githubPageLink}
-                className="color-3"
-                data-grid-class="item-3"
             />
             <ProjectTab
                 projectName={PROJECT_4.projectName}
                 projectDescription={PROJECT_4.projectDescription}
                 githubLink={PROJECT_4.githubLink}
                 githubPageLink={PROJECT_4.githubPageLink}
-                className="color-4"
-                data-grid-class="item-4"
             />
     </div>
     );
